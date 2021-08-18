916cd7d7eff31f68c01504611d1bcf054f922a78
fperazzo <fernando_perazzo@yahoo.com.br>
2021-08-18 14:49:15 -0300

    dg222

--- criatbdg.py
+++ criatbdg.py
@@ -8,17 +8,7 @@ try:
     # connect to the PostgreSQL server
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
-    # create table one by one
-    cur.execute("""CREATE TABLE dadosgerais (
-    id SERIAL PRIMARY KEY,
-    data timestamp NOT NULL,
-    acoes real NOT NULL,
-    fii real NOT NULL,
-    reits real NOT NULL,
-    rfixa real NOT NULL,
-    rvalor real NOT NULL,
-    stocks real NOT NULL,)
-    """)
+    cur.execute("CREATE TABLE dadosgerais (id SERIAL PRIMARY KEY, data timestamp NOT NULL, acoes real NOT NULL, fii real NOT NULL, reits real NOT NULL, rfixa real NOT NULL, rvalor real NOT NULL, stocks real NOT NULL);")
     # close communication with the PostgreSQL database server
     cur.close()
     # commit the changes
